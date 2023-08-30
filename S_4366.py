3b8b20a345db375a11ce4cbb3ed08ef6d92c3da0
Shin-3117 <tlswnd3127@gmail.com>
2023-08-30 16:39:31 +0900

    0830

--- S_4366.py
+++ S_4366.py
@@ -2,31 +2,71 @@
 import sys
 sys.stdin = open('./inputs/S_4366.txt','r', encoding='utf-8')
 
-def is23(INT):
-    i = 0
-    while True:
-        a = INT - 2**i
-
 
 for tc in range(int(input())):
-    B = input()
-    C = input()
-    BL = len(B)
-    CL = len(C)
+    binary = list(map(int,input().strip()))
+    trinary = list(map(int,input().strip()))
+    binaryLength = len(binary)
+    trinaryLength = len(trinary)
     
-    BV = 0
-    for i in range(BL):
-        BV += int(B[-i-1])*2**i
-    # print(BV)
-    CV = 0
-    for i in range(CL):
-        CV += int(C[-i-1])*3**i
-    # print(CV)
-    minus = abs(BV-CV)
-    # print(minus)
+    binaryTS10List = []
+    for tsIdx in range(binaryLength):
+        ts10 = 0
+        if binary[tsIdx] == 1:
+            binary[tsIdx] = 0
+            for i in range(binaryLength):
+                ts10 += binary[-i-1]*(2**i)
+            binaryTS10List.append(ts10)
+            binary[tsIdx] = 1
+        elif binary[tsIdx] == 0:
+            binary[tsIdx] = 1
+            for i in range(binaryLength):
+                ts10 += binary[-i-1]*(2**i)
+            binaryTS10List.append(ts10)
+            binary[tsIdx] = 0
 
-    is23(minus)
-
-    print(BV)
-    # B_max = 2**(BL)-1
-    # C_max = 3**(CL)-1
+    trinaryTS10List = []
+    for tsIdx in range(trinaryLength):
+        ts10 = 0
+        if trinary[tsIdx] == 0:
+            trinary[tsIdx] = 1
+            for i in range(trinaryLength):
+                ts10 += trinary[-i-1]*(3**i)
+            trinaryTS10List.append(ts10)
+            ts10 = 0
+            trinary[tsIdx] = 2
+            for i in range(trinaryLength):
+                ts10 += trinary[-i-1]*(3**i)
+            trinaryTS10List.append(ts10)
+            trinary[tsIdx] = 0
+        elif trinary[tsIdx] == 1:
+            trinary[tsIdx] = 0
+            for i in range(trinaryLength):
+                ts10 += trinary[-i-1]*(3**i)
+            trinaryTS10List.append(ts10)
+            ts10 = 0
+            trinary[tsIdx] = 2
+            for i in range(trinaryLength):
+                ts10 += trinary[-i-1]*(3**i)
+            trinaryTS10List.append(ts10)
+            trinary[tsIdx] = 1
+        elif trinary[tsIdx] == 2:
+            trinary[tsIdx] = 0
+            for i in range(trinaryLength):
+                ts10 += trinary[-i-1]*(3**i)
+            trinaryTS10List.append(ts10)
+            ts10 = 0
+            trinary[tsIdx] = 1
+            for i in range(trinaryLength):
+                ts10 += trinary[-i-1]*(3**i)
+            trinaryTS10List.append(ts10)
+            trinary[tsIdx] = 2
+    
+    # print(binaryTS10List)
+    # print(trinaryTS10List)
+    sol = 0
+    for num in binaryTS10List:
+        if num in trinaryTS10List:
+            sol = num
+            break
+    print(f'#{tc+1} {sol}')
