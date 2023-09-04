5b87e76ec4588deb84f7c2f01a7b82637bcd38d3
AbcLabFeShin <tlswnd3127@gmail.com>
2023-09-04 21:26:58 +0900

    0904 sol

--- 0. 자료구조/B_11723.py
+++ 0. 자료구조/B_11723.py
@@ -23,8 +23,7 @@ for _ in range(int(input())):
     except:
         # print(order)
         if order == 'all':
-            for i in range(1,21):
-                data.add(str(i))
+            data = set(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])
             # print(list(data))
         elif order == 'empty':
             data = set()
