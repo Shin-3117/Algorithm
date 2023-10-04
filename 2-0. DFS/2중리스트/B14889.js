bf5ca3ea5154fd942aeee2c53e2e651e587399d4
신현중 <qsc3117@naver.com>
2023-10-04 16:18:30 +0900

    add js B14889

---
+++ 2-0. DFS/2중리스트/B14889.js
@@ -0,0 +1,48 @@
+const sol = (input) => {
+    const N = +input[0]; // 첫 번째 줄
+    const halfN = N / 2;
+    //input의 2,3,4~ 줄 '0 1 2 3'을 [0,1,2,3]으로 변경
+    const stats = input.slice(1).map((str) => str.split(" ").map(Number));
+    console.log(stats)
+    const check = new Array(N).fill(0); // 파이썬의 check = [0]*N
+    let min = Number.MAX_SAFE_INTEGER; // 파이썬의 float('inf')와 같음
+    function dfs(L, K) {
+      if (L === halfN) { // 스타트팀에 N/2 명이 뽑혔다면
+        const sTeam = [];
+        const lTeam = [];
+        let sSum = (lSum = 0);
+        for (let i = 0; i < N; i++) {
+          if (check[i]) sTeam.push(i); // 체크 배열은 스타트 팀에 넣어주고, 체크 배열에 없으면 링크 팀에 넣어준다.
+          else lTeam.push(i);
+        }
+        for (let i = 0; i < halfN; i++) {
+          for (let j = i + 1; j < halfN; j++) { // (i,j), (j,i) 쌍을 계속 더해준다.
+            sSum = sSum + stats[sTeam[i]][sTeam[j]] + stats[sTeam[j]][sTeam[i]];
+            lSum = lSum + stats[lTeam[i]][lTeam[j]] + stats[lTeam[j]][lTeam[i]];
+          }
+        }
+        min = Math.min(min, Math.abs(sSum - lSum));
+        return;
+      }
+  
+      for (let i = K; i < N; i++) { // 체크 배열을 스타트 팀 구성에 사용한다.
+        check[i] = 1;
+        dfs(L + 1, i + 1);
+        check[i] = 0;
+      }
+    }
+    dfs(0, 0);
+    return min;
+  };
+  
+  // 백준에서 입력을 받는 코드
+  const input = [];
+  require("readline")
+    .createInterface(process.stdin, process.stdout)
+    .on("line", (line) => {
+      input.push(line);
+    })
+    .on("close", () => {
+      console.log(sol(input));
+      process.exit();
+    });
