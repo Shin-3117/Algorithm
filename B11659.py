459ceb23a893a42b5a211b1ae6ec7ef50787f879
Shin-3117 <tlswnd3127@gmail.com>
2023-12-20 01:59:15 +0900

    add B11659

---
+++ B11659.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('./inputs3/B11659.txt','r')
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+N_List = list(map(int, input().split()))
+
+sum_stack = [0]*(N+1)
+for idx in range(N):
+  sum_stack[idx+1] = sum_stack[idx] + N_List[idx]
+
+for _ in range(M):
+  i,j = map(int, input().split())
+  ans = sum_stack[j]-sum_stack[i-1]
+  print(ans)
--- S2382.py
+++
@@ -1,2 +0,0 @@
-import sys
-sys.stdin = open('./inputs2/S2382.txt','r')
